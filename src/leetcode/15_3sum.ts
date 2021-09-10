1d41ab08e3bf7010ed367053694bbb07ff45380a
blacktoast <dbstjd100@naver.com>
2021-09-10 17:07:32 +0900

    15번 해결? 애메함

--- src/leetcode/15_3sum.ts
+++ src/leetcode/15_3sum.ts
@@ -1,25 +1,38 @@
 function threeSum(nums: number[]): number[][] {
   let zero = [];
-  let result = [];
-  let zeroTrue = false;
+  let l = nums.length;
+  let before = [];
+
   nums = nums.sort(function (f, s) {
     return f - s;
   });
-  for (let i = 0; i < nums.length; i++) {
-    let zero = [];
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let left: number, right: number;
+    left = i + 1;
+    right = l - 1;
 
-    for (let j = 1; j < nums.length - (i + 1); j++) {
+    while (left < right) {
       let sum = 0;
-      sum = nums[i] + nums[i + j] + nums[i + (j - 1)];
-      if (sum === 0) {
-        zero.push(nums[i], nums[i + j], nums[i + (j - 1)]);
+      sum = nums[i] + nums[left] + nums[right];
+      before = [nums[i], nums[left], nums[right]];
+
+      // console.log("tt  ", i, nums[i], nums[left], nums[right], sum);
+      if (sum !== 0) {
+        sum < 0 ? (left += 1) : (right -= 1);
+      } else {
+        zero.push([nums[i], nums[left], nums[right]]);
+        while (left < right && nums[left] === nums[left + 1]) left += 1;
+        while (left < right && nums[right] === nums[right - 1]) right -= 1;
+        // console.log(left, right);
+        left += 1;
+        right -= 1;
       }
     }
-    result.push(zero);
-    //console.log("   " + zero);
   }
-  // console.log(nums);
+  //console.log(zero);
+  return zero;
 }
 //투포인터로 n2제곱으로
 //
-threeSum([-1, 0, 1, 2, -1, -4]);
+threeSum([-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6]);
