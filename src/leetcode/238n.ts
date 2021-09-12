616f3b9026e2876f75813ba2b07d29ddd7ef9c91
blacktoast <dbstjd100@naver.com>
2021-09-12 17:29:49 +0900

    238해결

--- src/leetcode/238n.ts
+++ src/leetcode/238n.ts
@@ -1,11 +1,19 @@
 function productExceptSelf(nums: number[]): number[] {
 let out=[];
 let p=1;
-
+for(let i=0; i<nums.length; i++){
+    out.push(p);
+    p=p*nums[i];
+}
+p=1;
+for(let i=nums.length-1; i>=0; i--) {
+out[i]=p*out[i];
+p=p*nums[i];
+}
+//onsole.log(out);
 
     
- let result: number[] =[];
-    return result;
+    return out;
 };
 
-console.log(productExceptSelf([1,2,3,4]));
+console.log(productExceptSelf([-1,1,0,-3,3]));
