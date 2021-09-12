189b5f90279fa0fda081a8494c0fcfc62329d11e
blacktoast <dbstjd100@naver.com>
2021-09-12 14:12:54 +0900

    238번 푸는중...

--- src/leetcode/238n.ts
+++ src/leetcode/238n.ts
@@ -1,7 +1,11 @@
 function productExceptSelf(nums: number[]): number[] {
- 
+let out=[];
+let p=1;
+
 
     
  let result: number[] =[];
     return result;
 };
+
+console.log(productExceptSelf([1,2,3,4]));
