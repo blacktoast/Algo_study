7f4deed7110c2bd1424b425182d4dbf6e31933fc
blacktoast <dbstjd100@naver.com>
2021-09-13 14:48:03 +0900

    121해결

--- src/leetcode/121n.ts
+++ src/leetcode/121n.ts
@@ -1,12 +1,36 @@
 function maxProfit(prices: number[]): number {
-  let result;
+  let result, now, profit, min;
   result = 0;
-  return result;
+  min = 10000;
+  profit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    now = prices[i];
+    min - now > 0 ? (min = now) : (result = now - min);
+    profit = Math.max(profit, result);
+  }
+  return profit;
 }
 
-//console.log(maxProfit([7,6,4,3,1]));
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
 /*
 순서를 바꿀순 x
 올라가면서  max min 값을 비교 하고 인덱스 값을 비교
+Fast answer
+    let profit = 0;
+    
+    let currentMin = prices[0];
+    
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < currentMin) {
+            currentMin = prices[i];
+        } else {
+            let localProfit = prices[i] - currentMin;
 
+            if (localProfit > profit) {
+                profit = localProfit;
+            }
+        }
+    }
+    
+    return profit;
  */
