1bb22811de29ac9f60cc5cf0bb7f699eba1504fd
blacktoast <dbstjd100@naver.com>
2021-09-13 14:38:39 +0900

    121문제 해결중

---
+++ src/leetcode/121n.ts
@@ -0,0 +1,12 @@
+function maxProfit(prices: number[]): number {
+  let result;
+  result = 0;
+  return result;
+}
+
+//console.log(maxProfit([7,6,4,3,1]));
+/*
+순서를 바꿀순 x
+올라가면서  max min 값을 비교 하고 인덱스 값을 비교
+
+ */
