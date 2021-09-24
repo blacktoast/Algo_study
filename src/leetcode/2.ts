2cc0751b3689db2c92645584fcc701284259d499
blacktoast <dbstjd100@naver.com>
2021-09-24 15:37:21 +0900

    leetcode 2번 푸는중..

---
+++ src/leetcode/2.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  let head = new ListNode(0);
+  let root = head;
+
+  let carry = 0;
+  while (l1 || l2) {}
+  return root.next;
+}
