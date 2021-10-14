5fb30aa62aeb628aa57a9cf3d6af0eab9a4215b9
blacktoast <dbstjd100@naver.com>
2021-10-14 21:18:27 +0900

    망망

--- rustAlgo/src/main.rs
+++ rustAlgo/src/main.rs
@@ -1,19 +1,35 @@
-use std::convert::TryInto;
-
+use std::{convert::TryInto};
+/*
+틀려먹은 방법이였다.
+벡터로 엌[어케 해야할듯]
+*/
 fn main() {
-    let test=vec![2,3,5];
-    plus_one(test);
+    let test=vec![7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6];
+    let a=plus_one(test);
+    println!("{:?}",a);
 }
 pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
 
     let mut test=Vec::new();
     let len=digits.len()-1;
-    let mut sum:i32=0;
+    let mut sum:u128=0;
     for (i,t) in digits.iter().enumerate(){
-        sum+=t*(10_i32.pow((len-i).try_into().unwrap()));
+        sum+=(*t as u128)*(10_u128.pow((len-i) as u32));
     }
-    println!("{}",sum);
-    test
+    sum=sum+1;
+    
+    let len=sum.to_string().len();
+    for i in 1..len+2 {
+        let a:u128=sum/10_u128.pow((len+1-i).try_into().unwrap());
+        let a=a as i32;
+        test.push(a);
+        println!("{}",a);
+        sum=sum%10_u128.pow((len+1-i).try_into().unwrap());
+        println!("{}",sum);
+
+    }
+    
+    test[1..].to_vec()
 }
 
 
