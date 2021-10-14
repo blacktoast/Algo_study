42c84ea2f610b97c58646f98212fd773c45cc588
blacktoast <dbstjd100@naver.com>
2021-10-14 08:09:07 +0000

    푸는중

--- rustAlgo/src/main.rs
+++ rustAlgo/src/main.rs
@@ -1,3 +1,19 @@
+use std::convert::TryInto;
+
 fn main() {
-    println!("Hello, world!");
+    let test=vec![2,3,5];
+    plus_one(test);
 }
+pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
+
+    let mut test=Vec::new();
+    let len=digits.len()-1;
+    let mut sum:i32=0;
+    for (i,t) in digits.iter().enumerate(){
+        sum+=t*(10_i32.pow((len-i).try_into().unwrap()));
+    }
+    println!("{}",sum);
+    test
+}
+
+
