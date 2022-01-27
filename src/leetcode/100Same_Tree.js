66dd7629e2709452772fdb2946ebe6b4df21c36e
blacktoast <dbstjd100@naver.com>
2022-01-27 06:36:04 +0000

    same tree 해결

---
+++ src/leetcode/100Same_Tree.js
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * // bfs 큐로 해결
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+  console.log("===============");
+  if (!p && !q) {
+    console.log(p);
+    return true;
+  }
+
+  if ((!p && q) || (p && !q) || p.val !== q.val) {
+    return false;
+  }
+
+  let will = [];
+  let will2 = [];
+  will.push(p);
+  will2.push(q);
+
+  while (will.length || will2.length) {
+    let [pf, qf] = [will.shift(), will2.shift()];
+    // console.log(pf,qf)
+    if (!pf || !qf) {
+      if (!pf !== !qf) return false;
+    } else {
+      if (pf.val !== qf.val) return false;
+      will.push(pf.left);
+      will.push(pf.right);
+      will2.push(qf.left);
+      will2.push(qf.right);
+      // console.log(will,will2)
+    }
+  }
+
+  if (!will.length && !will.length) {
+    return true;
+  }
+  return false;
+};
