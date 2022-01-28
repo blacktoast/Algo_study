15d077c336e56936ccab2b36ebaf82a215e0abd2
blacktoast <dbstjd100@naver.com>
2022-01-28 08:46:29 +0000

    11번 문제해결중

---
+++ src/leetcode/11. Container With Most Water.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let [start, end] = [0, height.length - 1];
+  let max = -1;
+  while (start <= end) {}
+  console.log(height);
+};
+
+maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]);
