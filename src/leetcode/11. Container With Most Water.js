e2ee1fd5fd15b6ab34d502bee10771dea43f05f5
blacktoast <dbstjd100@naver.com>
2022-02-03 07:23:41 +0000

    11번 해결 - 투포인터 문제

--- src/leetcode/11. Container With Most Water.js
+++ src/leetcode/11. Container With Most Water.js
@@ -5,8 +5,18 @@
 var maxArea = function (height) {
   let [start, end] = [0, height.length - 1];
   let max = -1;
-  while (start <= end) {}
-  console.log(height);
+  let containHeight = 0;
+  let width = 0;
+  while (start < end) {
+    height[start] < height[end]
+      ? (containHeight = height[start])
+      : (containHeight = height[end]);
+
+    max = Math.max(max, (end - start) * containHeight);
+
+    height[start] < height[end] ? start++ : end--;
+  }
+  return max;
 };
 
 maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]);
